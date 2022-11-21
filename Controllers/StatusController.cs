8393dbe3afce8fc0cbd6d56cd7b31f409ebc6e08
willis <wilkes861219@gmail.com>
2022-11-21 15:07:41 +0800

    add statusAPI

---
+++ Controllers/StatusController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bike_Backend.Controllers
+{
+    /// <summary>
+    /// 拿來驗證Token是否過期
+    /// </summary>
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatusController : ControllerBase
+    {
+        /// <summary>
+        /// 驗證Token
+        /// </summary>
+        [HttpGet]
+        public bool status()
+        {
+            return true;
+        }
+    }
+}
